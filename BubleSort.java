f1fa854f357673c5d6239801dd5a5d61b14a6094
hp <priyanshik@222gmail.com>
2024-01-18 19:42:44 +0530

    first

---
+++ BubleSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+public class BubleSort {
+    public static void main(String[] args) {
+     int arr[] = new int[]{2,4,5,1,8};
+     System.out.println("elements Before sorting ");
+     for (int i = 0; i < arr.length; i++){
+         int v = arr[i];
+         System.out.print(v);
+     }
+     System.out.println();
+     System.out.println("after sorting");
+     bubblesort(arr); 
+    }
+    public static void bubblesort(int [] arr) {
+     int temp;
+     for(int i = 0; i <arr.length; i++)    
+     {    
+        for(int j=0; j<arr.length-1; j++) {            
+            if(arr[j] > arr[j + 1]) {
+                 temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp; 
+            } 
+        }
+    }System.out.println(Arrays.toString(arr));     
+}
+}
+     
+    
+    
+
---
+++ MinMaxSum.java
@@ -0,0 +1,37 @@
+public class MinMaxSum {
+    public static void main(String[] args) {
+        
+        int Arr[] = {2,3,6,62,53};
+        int min, max;
+        max = Arr[0];
+        min = Arr[0];
+
+
+            for (int i = 0  ; i < Arr.length ; i++) {
+                if (Arr[i] < min)
+                    {
+                    min = Arr[i];
+                    }
+                    if (Arr[i] > max)
+                    {
+                        max = Arr[i];   
+                    }          
+            }
+             System.out.println(min);
+             System.out.println(max);
+             int SumOfminmax = sumOfminmax(min, max);
+             System.out.println(SumOfminmax); 
+             
+             
+    }
+
+    private static int sumOfminmax(int min, int max) {
+        return min + max;
+    
+    }
+}
+
+
+
+    
+
